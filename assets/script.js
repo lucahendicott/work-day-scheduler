25d6eea2e7a960e34244ee5c7c2128587f1d70bf
Lucah Endicott <lucahdavid88@gmail.com>
2020-10-28 12:45:07 -0700

    update html classes

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   
 let timeDate = document.querySelector("#currentDay")
+let currentHour = document.querySelectorAll("textarea")
+let saveBtn = document.querySelector("button")
+let timeNow = parseInt(moment().format("H"))
 
 timeDate.textContent = moment().format("dddd, MMMM Do YYYY");
 
-let timeNow = parseInt(moment().format("H"))
 
-let currentHour = document.querySelectorAll(".time-block")
-
-console.log(typeof timeNow)
+// console.log(typeof timeNow)
 
 for (let i = 0; i < currentHour.length; i++) {
     if(parseInt(currentHour[i].id.slice(4)) === timeNow) {
@@ -21,5 +21,16 @@ for (let i = 0; i < currentHour.length; i++) {
         $(currentHour[i]).addClass("past")
     }
 
+
+    // function saveToLocalStorage (){
+    //     localStorage.setItem(timeNow, JSON.stringify(currentHour.textValue))
+    // }
+    // function getFromLocalStorage (){
+    //     JSON.parse(localStorage.getItem(currentHour.textValue))
+    // }
+    // window.localStorage.setItem(JSON.stringify())
+    // JSON.parse(localStorage.getItem())
+
+    saveBtn.addEventListener("click",saveToLocalStorage)
 
   });
