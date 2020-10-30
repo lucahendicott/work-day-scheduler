6fb6b88e775299d25871eb9ce8f109c69e27e5b5
Lucah Endicott <lucahdavid88@gmail.com>
2020-10-30 14:05:47 -0700

    add readme and js psuedo code

---
+++ README.md
@@ -0,0 +1,31 @@
+# Work Day Planner
+
+This is a simple work day planner application. When the user launches the app in their browser, they are presented with time blocks from 8am - 7pm and the top of the page displays the current date. 
+
+The time blocks are color coded by time. whichever hour it currently is for the user, that block is red. Any blocks in the future are green and blocks in the past are gray. 
+
+The applications function is simply saving whatever the user enters into any text area to local storage upon clicking the "save" button. It also recieves all the saved local storage items upon refreshing the page. That way it holds all their plans for the day even if they close the page.
+
+## Under The Hood
+
+The directory for this app includes an HTML, CSS and JS file. The HTML is linked to bootsrap and laid out with some simple divs that hold the time, a text area and a save button for each time block. 
+
+In the JS file, I've motly used vanilla JS along with a splash of jQuery syntax. The funcitons to change the text block colors are in this file, they call to add the propper classes created in CSS to recieve the correct colors for the text blocks according to time. 
+
+I used moment.js to display the current date and to track each row by hour to display the proper colors in the text areas. 
+
+I created a function to save to local storage when you click the save button. Also to get from local storage so that the text blocks hold whatever the user saves in them upon reloading the application.
+
+## Getting Started 
+
+This application runs as is in the browser. No downloads or installs needed.
+
+## Resources
+
+[Moment Js Display Docs](https://momentjs.com/docs/#/displaying/)<br>
+[Changing Background Colors](https://stackoverflow.com/questions/46972901/changing-the-background-color-at-different-hours-of-the-day-in-javascript/46973019)<br>
+[Local Storage Help](https://www.taniarascia.com/how-to-use-local-storage-with-javascript/)
+
+## Deployed App
+
+[Let's plan your work day!](https://lucahendicott.github.io/work-day-scheduler/)
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
-// $(document).ready(function() {
+$(document).ready(function() {
+
+    //set up variables with query selectors
 let timeDate = document.querySelector("#currentDay")
 let currentHour = document.querySelectorAll("textarea")
-let saveBtn = document.querySelector(".btnClass")
 let timeNow = parseInt(moment().format("H"))
-//add class "userInput" to text area div
-function primaryFunk(){
-getFromLocalStorage()
 
-$(currentHour).addClass("userInput")
+//primary function kicking things off 
+function primaryFunk(){
+
+getFromLocalStorage()
 
 timeDate.textContent = moment().format("dddd, MMMM Do YYYY");
 
+//loop to run through all text areas followed by conditionals to set the color of each text area according to the current time.
  for(let i = 0; i < currentHour.length; i++) {
     if(parseInt(currentHour[i].id.slice(4)) === timeNow) {
         $(currentHour[i]).addClass("present")
@@ -22,35 +24,32 @@ timeDate.textContent = moment().format("dddd, MMMM Do YYYY");
         $(currentHour[i]).addClass("past")
     }
 
-    var elements = document.querySelectorAll("button");   
-    for(var i = 0, len = elements.length; i < len; i++) {   
-        // console.log(i)
+    //for loop to add event listener on all buttons
+    let elements = document.querySelectorAll("button");   
+    for(let i = 0, len = elements.length; i < len; i++) {   
 
-        elements[i].addEventListener("click", saveToLocalStorage);
-
+    elements[i].addEventListener("click", saveToLocalStorage);
     }
     }
 
+    //function to save the value in the text area to local storage
     function saveToLocalStorage() {
-    //   console.log(this)
-      var p = this.id
-      var x = "#hour" + p
-      var w = document.querySelector(x).value
+      let p = this.id
+      let x = "#hour" + p
+      let w = document.querySelector(x).value
       localStorage.setItem(x, w)
-        
     }
 
+    //function to get saved items from local storage, keeping text area value displayed when pafe refreshes
     function getFromLocalStorage(){
         for (let i = 9; i <= 19; i++) {
-        var x = "#hour" + i
+        let x = "#hour" + i
         if(localStorage.getItem(x)){
-            var w = (localStorage.getItem(x))
+            let w = (localStorage.getItem(x))
             let p = document.querySelector(x)
             p.value = w
-            console.log(w)
         }
-        }
-        
+        }  
     }
-
     primaryFunk()
+})
